5313bbdeaad00961b6edf1bbd9deb31e92a64a96
Mahalakshmi Chopperla <109203613+Mahachopperla@users.noreply.github.com>
2026-01-02 01:05:11 +0530

    Clean up comments in output.tf
    Removed comments regarding VPC output handling responsibilities.

--- output.tf
+++ output.tf
@@ -1,8 +1,4 @@
-# after executing vpc module , this vpc will be created in aws along with that vpc_id will be sent as output
-# this output should be captured and stored in ssm parameter store while creating vpc .
 
-#capturing the output and storing it in ssm parameter store is the responsibilty of employee who is creating this
-# vpc instance
 
 
 output "vpc_id" {
